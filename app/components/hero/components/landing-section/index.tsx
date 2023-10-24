b76da41d68577f1e9008b7cabd5df8bdd2a03441
bonface221 <mainaboniface221@gmail.com>
2023-10-24 21:21:39 +0300

    home section and navbar

--- app/components/hero/components/landing-section/index.tsx
+++ app/components/hero/components/landing-section/index.tsx
@@ -1,42 +1,44 @@
-import { Button, Flex, Heading, Stack } from "@chakra-ui/react";
+import { Button, Flex, Grid, GridItem, Heading, Stack } from "@chakra-ui/react";
 
 const LandingSection = () => {
   return (
-    <Flex h="88vh">
-      <Stack gap="50px">
-        <Heading
-          as="h3"
-          color="brand.main"
-          fontSize="15px"
-          letterSpacing="6.6px"
-          lineHeight="22px"
-          fontWeight={400}
-          fontStyle="normal"
-        >
-          Makeup Artist
-        </Heading>
-        <Heading
-          as="h1"
-          color="brand.white"
-          fontSize="70px"
-          lineHeight="26px"
-          letterSpacing="12.6px"
-          fontWeight={400}
-          fontStyle="normal"
-        >
-          IAN KIITHYA
-        </Heading>
-        <Button
-          w="116px"
-          borderRadius="none"
-          bg="brand.main"
-          color="brand.white"
-          p="12px 32px"
-        >
-          About Me
-        </Button>
-      </Stack>
-    </Flex>
+    <Grid h="90vh" templateRows="repeat(5, 1fr)">
+      <GridItem gridRowStart={2}>
+        <Stack gap="50px">
+          <Heading
+            as="h3"
+            color="brand.main"
+            fontSize="15px"
+            letterSpacing="6.6px"
+            lineHeight="22px"
+            fontWeight={400}
+            fontStyle="normal"
+          >
+            Makeup Artist
+          </Heading>
+          <Heading
+            as="h1"
+            color="brand.white"
+            fontSize="70px"
+            lineHeight="26px"
+            letterSpacing="12.6px"
+            fontWeight={400}
+            fontStyle="normal"
+          >
+            IAN KIITHYA
+          </Heading>
+          <Button
+            w="116px"
+            borderRadius="none"
+            bg="brand.main"
+            color="brand.white"
+            p="12px 32px"
+          >
+            About Me
+          </Button>
+        </Stack>
+      </GridItem>
+    </Grid>
   );
 };
 
